91095e0628ea027be98147db12c3ed4a97de1d14
xndcmd <xndcmd@gmail.com>
2022-06-24 09:01:54 -0700

    added getter functions for Employee class

--- Employee.js
+++ Employee.js
@@ -3,5 +3,28 @@
 // ------------------------------- //
 
 
+class Employee {
+    constructor(id, name, email) {
+        this.name = name,
+        this.id = id, 
+        this.email = email
+    }
+    getName() {
+        return this.name
+    }
+
+    getId() {
+        return this.id
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getRole() {
+        return 'Employee'
+    }
+
+}
 
 
