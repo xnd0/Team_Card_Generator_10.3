e718f635762b2ff80937915ad27bd7a25f1c9dae
xndcmd <xndcmd@gmail.com>
2022-06-24 09:53:37 -0700

    added Manager class extension & export module

--- Manager.js
+++ Manager.js
@@ -3,3 +3,17 @@
 // ------------------------------ //
 
 
+const Employee = require("./Employee")
+
+class Manager extends Employee {
+    constructor(id, name, email, officeNum) {
+        super(id, name, email);
+    this.officeNumber = officeNum;    
+    }
+    getRole() {
+        return "Manager";
+    }
+
+}
+
+module.exports = Manager
