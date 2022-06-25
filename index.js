173f10a9ab479b061cab19089f7f538e04fe744b
xndcmd <xndcmd@gmail.com>
2022-06-25 15:58:54 -0700

    added inquirer questions

--- index.js
+++ index.js
@@ -3,6 +3,53 @@
 // ---------------------------- //
 
 const inquirer = require("inquirer");
+const Manager = require("./Manager");
 
 
 
+
+const init = () => {
+    const createManager = () => {
+        inquirer.prompt(
+            [
+                {
+                    type: 'input',
+                    message: 'What is the ID number?',
+                    name: 'id',
+                },
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: 'What is the managers name?',
+                },
+
+                {
+                    type: 'input',
+                    message: 'What is your preferred method of communication?',
+                    name: 'email',
+                },
+                {
+                    type: 'input',
+                    message: 'What is the office number?',
+                    name: 'officeNumber',
+                },
+            ])
+            .then(answers => {
+                const manager = new Manager(
+                    answers.id,
+                    answers.name,
+                    answers.email,
+                    answers.officeNumber
+                );
+                teamMemberObjArr.push(manager)
+                addEmployees()
+            })
+
+    };
+
+    createManager();
+};
+
+
+
+init();
